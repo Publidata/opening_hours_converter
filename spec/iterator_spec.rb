ab2c2b78964254e95e6ebe290fe0e5bdd19968cd
Martin Ziserman <martin@mbp-de-martin.home>
2018-03-15 15:03:31 +0100

    test ph with year

--- spec/iterator_spec.rb
+++ spec/iterator_spec.rb
@@ -6,33 +6,33 @@ RSpec.describe OpeningHoursConverter::Iterator, "state" do
   end
   it "state" do
     expect(OpeningHoursConverter::Iterator.new.state("off")).to be false
-    expect(OpeningHoursConverter::Iterator.new.state("PH 16:00-17:00", Time.new(2017,1,1,16,30))).to eql({start: Time.new(2017,1,1,16,00), end: Time.new(2017,1,1,17,00)})
-    expect(OpeningHoursConverter::Iterator.new.state("Su,PH 16:00-17:00", Time.new(2017,1,1,16,30))).to eql({start: Time.new(2017,1,1,16,00), end: Time.new(2017,1,1,17,00)})
-    expect(OpeningHoursConverter::Iterator.new.state("Mo,PH 16:00-17:00", Time.new(2017,1,1,16,30))).to eql({start: Time.new(2017,1,1,16,00), end: Time.new(2017,1,1,17,00)})
-    expect(OpeningHoursConverter::Iterator.new.state("Fr 16:00-17:00", Time.new(2017,10,13,16,30))).to eql({start: Time.new(2017,10,13,16,00), end: Time.new(2017,10,13,17,00)})
-    expect(OpeningHoursConverter::Iterator.new.state("Fr 16:00-17:00", Time.new(2017,10,13,17,30))).to be false
+    expect(OpeningHoursConverter::Iterator.new.state("2017 PH 16:00-17:00", Time.new(2017,1,1,16,30))).to eql({start: Time.new(2017,1,1,16,00), end: Time.new(2017,1,1,17,00)})
+    expect(OpeningHoursConverter::Iterator.new.state("2017 Su,PH 16:00-17:00", Time.new(2017,1,1,16,30))).to eql({start: Time.new(2017,1,1,16,00), end: Time.new(2017,1,1,17,00)})
+    expect(OpeningHoursConverter::Iterator.new.state("2017 Mo,PH 16:00-17:00", Time.new(2017,1,1,16,30))).to eql({start: Time.new(2017,1,1,16,00), end: Time.new(2017,1,1,17,00)})
+    expect(OpeningHoursConverter::Iterator.new.state("2017 Fr 16:00-17:00", Time.new(2017,10,13,16,30))).to eql({start: Time.new(2017,10,13,16,00), end: Time.new(2017,10,13,17,00)})
+    expect(OpeningHoursConverter::Iterator.new.state("2017 Fr 16:00-17:00", Time.new(2017,10,13,17,30))).to be false
   end
   it "next_state" do
     expect(OpeningHoursConverter::Iterator.new.next_state("off")).to be false
-    expect(OpeningHoursConverter::Iterator.new.next_state("PH 16:00-17:00", Time.new(2017,1,1,16,30))).to eql({end: Time.new(2017,1,1,17,00)})
-    expect(OpeningHoursConverter::Iterator.new.next_state("Su,PH 16:00-17:00", Time.new(2017,1,1,16,30))).to eql({end: Time.new(2017,1,1,17,00)})
-    expect(OpeningHoursConverter::Iterator.new.next_state("Mo,PH 16:00-17:00", Time.new(2017,1,1,16,30))).to eql({end: Time.new(2017,1,1,17,00)})
-    expect(OpeningHoursConverter::Iterator.new.next_state("Fr 16:00-17:00", Time.new(2017,10,13,16,30))).to eql({end: Time.new(2017,10,13,17,00)})
-    expect(OpeningHoursConverter::Iterator.new.next_state("Fr 16:00-17:00", Time.new(2017,10,13,17,30))).to eql({start: Time.new(2017,10,20,16,00)})
+    expect(OpeningHoursConverter::Iterator.new.next_state("2017 PH 16:00-17:00", Time.new(2017,1,1,16,30))).to eql({end: Time.new(2017,1,1,17,00)})
+    expect(OpeningHoursConverter::Iterator.new.next_state("2017 Su,PH 16:00-17:00", Time.new(2017,1,1,16,30))).to eql({end: Time.new(2017,1,1,17,00)})
+    expect(OpeningHoursConverter::Iterator.new.next_state("2017 Mo,PH 16:00-17:00", Time.new(2017,1,1,16,30))).to eql({end: Time.new(2017,1,1,17,00)})
+    expect(OpeningHoursConverter::Iterator.new.next_state("2017 Fr 16:00-17:00", Time.new(2017,10,13,16,30))).to eql({end: Time.new(2017,10,13,17,00)})
+    expect(OpeningHoursConverter::Iterator.new.next_state("2017 Fr 16:00-17:00", Time.new(2017,10,13,17,30))).to eql({start: Time.new(2017,10,20,16,00)})
   end
   it "next_period" do
     expect(OpeningHoursConverter::Iterator.new.next_period("off")).to be false
-    expect(OpeningHoursConverter::Iterator.new.next_period("PH 16:00-17:00", Time.new(2017,1,1,16,30))).to eql({start: Time.new(2017,4,14,16,00), end: Time.new(2017,4,14,17,00)})
-    expect(OpeningHoursConverter::Iterator.new.next_period("Su,PH 16:00-17:00", Time.new(2017,1,1,16,30))).to eql({start: Time.new(2017,1,8,16,00), end: Time.new(2017,1,8,17,00)})
-    expect(OpeningHoursConverter::Iterator.new.next_period("Mo,PH 16:00-17:00", Time.new(2017,1,1,16,30))).to eql({start: Time.new(2017,1,2,16,00), end: Time.new(2017,1,2,17,00)})
-    expect(OpeningHoursConverter::Iterator.new.next_period("Fr 16:00-17:00", Time.new(2017,10,13,16,30))).to eql({start: Time.new(2017,10,20,16,00), end: Time.new(2017,10,20,17,00)})
-    expect(OpeningHoursConverter::Iterator.new.next_period("Fr 16:00-17:00", Time.new(2017,10,13,17,30))).to eql({start: Time.new(2017,10,20,16,00), end: Time.new(2017,10,20,17,00)})
+    expect(OpeningHoursConverter::Iterator.new.next_period("2017 PH 16:00-17:00", Time.new(2017,1,1,16,30))).to eql({start: Time.new(2017,4,14,16,00), end: Time.new(2017,4,14,17,00)})
+    expect(OpeningHoursConverter::Iterator.new.next_period("2017 Su,PH 16:00-17:00", Time.new(2017,1,1,16,30))).to eql({start: Time.new(2017,1,8,16,00), end: Time.new(2017,1,8,17,00)})
+    expect(OpeningHoursConverter::Iterator.new.next_period("2017 Mo,PH 16:00-17:00", Time.new(2017,1,1,16,30))).to eql({start: Time.new(2017,1,2,16,00), end: Time.new(2017,1,2,17,00)})
+    expect(OpeningHoursConverter::Iterator.new.next_period("2017 Fr 16:00-17:00", Time.new(2017,10,13,16,30))).to eql({start: Time.new(2017,10,20,16,00), end: Time.new(2017,10,20,17,00)})
+    expect(OpeningHoursConverter::Iterator.new.next_period("2017 Fr 16:00-17:00", Time.new(2017,10,13,17,30))).to eql({start: Time.new(2017,10,20,16,00), end: Time.new(2017,10,20,17,00)})
   end
   it "is_opened" do
     expect(OpeningHoursConverter::Iterator.new.is_opened?("off")).to be false
-    expect(OpeningHoursConverter::Iterator.new.is_opened?("PH 16:00-17:00", Time.new(2017,10,13,16,30))).to be false
-    expect(OpeningHoursConverter::Iterator.new.is_opened?("PH 16:00-17:00", Time.new(2017,1,1,16,30))).to be true
-    expect(OpeningHoursConverter::Iterator.new.is_opened?("Fr 16:00-17:00", Time.new(2017,10,13,16,30))).to be true
-    expect(OpeningHoursConverter::Iterator.new.is_opened?("Fr 16:00-17:00", Time.new(2017,10,13,17,30))).to be false
+    expect(OpeningHoursConverter::Iterator.new.is_opened?("2017 PH 16:00-17:00", Time.new(2017,10,13,16,30))).to be false
+    expect(OpeningHoursConverter::Iterator.new.is_opened?("2017 PH 16:00-17:00", Time.new(2017,1,1,16,30))).to be true
+    expect(OpeningHoursConverter::Iterator.new.is_opened?("2017 Fr 16:00-17:00", Time.new(2017,10,13,16,30))).to be true
+    expect(OpeningHoursConverter::Iterator.new.is_opened?("2017 Fr 16:00-17:00", Time.new(2017,10,13,17,30))).to be false
   end
 end
