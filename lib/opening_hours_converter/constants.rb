3a351cefe51653ba78f1844e8a39bf9be6d7efa8
MZiserman <martinziserman@gmail.com>
2019-05-21 17:09:45 +0200

    lint constants

--- lib/opening_hours_converter/constants.rb
+++ lib/opening_hours_converter/constants.rb
@@ -9,13 +9,13 @@ module OpeningHoursConverter
       VENDREDI: 4,
       SAMEDI: 5,
       DIMANCHE: 6
-    }
-    OSM_DAYS = [ "Mo", "Tu", "We", "Th", "Fr", "Sa", "Su" ]
-    IRL_DAYS = [ "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche" ]
-    OSM_MONTHS = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ]
-    IRL_MONTHS = [ "Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre",
-      "Octobre", "Novembre", "Décembre" ]
-    MONTH_END_DAY = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
+    }.freeze
+    OSM_DAYS = %w[Mo Tu We Th Fr Sa Su].freeze
+    IRL_DAYS = %w[Lundi Mardi Mercredi Jeudi Vendredi Samedi Dimanche].freeze
+    OSM_MONTHS = %w[Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec].freeze
+    IRL_MONTHS = %W[Janvier Fevrier Mars Avril Mai Juin Juillet Aout Septembre
+                    Octobre Novembre D\u00E9cembre].freeze
+    MONTH_END_DAY = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31].freeze
     MINUTES_MAX = 1440
     DAYS_MAX = 6
     YEAR_DAYS_MAX = 365
