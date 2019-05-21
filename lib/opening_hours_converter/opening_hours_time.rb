837a5f8a941995c99e81807947f22fd013c51526
MZiserman <martinziserman@gmail.com>
2019-05-21 17:04:29 +0200

    lint time

--- lib/opening_hours_converter/opening_hours_time.rb
+++ lib/opening_hours_converter/opening_hours_time.rb
@@ -2,16 +2,14 @@ module OpeningHoursConverter
   class OpeningHoursTime
     attr_reader :start, :end, :priority
 
-    def initialize(minute_start=nil, minute_end=nil)
+    def initialize(minute_start = nil, minute_end = nil)
       @start = minute_start
-      unless minute_start == minute_end
-        @end = minute_end
-      end
+      @end = minute_end unless minute_start == minute_end
     end
 
     def get
-      return "off" if (@start.nil? && @end.nil?)
-      "#{time_string(@start)}#{@end.nil? ? "" : "-#{time_string(@end)}"}"
+      return 'off' if @start.nil? && @end.nil?
+      "#{time_string(@start)}#{@end.nil? ? '' : "-#{time_string(@end)}"}"
     end
 
     def equals(t)
@@ -20,9 +18,9 @@ module OpeningHoursConverter
 
     def time_string(minutes)
       fminutes = minutes.to_f
-      h = (fminutes/60).floor.to_i
-      m = (fminutes%60).to_i
-      "#{h < 10 ? "0" : ""}#{h}:#{m < 10 ? "0" : ""}#{m}"
+      h = (fminutes / 60).floor.to_i
+      m = (fminutes % 60).to_i
+      "#{h < 10 ? '0' : ''}#{h}:#{m < 10 ? '0' : ''}#{m}"
     end
   end
 end
